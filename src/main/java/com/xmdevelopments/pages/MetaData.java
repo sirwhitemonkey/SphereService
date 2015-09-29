89409d8ecd1474e74664883566170de7f134ad7b
Rommel D Sumpo <rommel.sumpo@certussolutions.com>
2015-09-29 18:02:47 +1300

    Implement date_last_updated in the metadata

--- src/main/java/com/xmdevelopments/pages/MetaData.java
+++ src/main/java/com/xmdevelopments/pages/MetaData.java
@@ -1,15 +1,21 @@
 package com.xmdevelopments.pages;
 
+import java.util.Date;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 
 public class MetaData {
 
 	private Page<Object> page;
+	private long date_last_updated;
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public MetaData(Page page) {
 		this.page = page;
+		Date date = new java.util.Date(); 
+		long unixTime = (long)date.getTime()/1000;
+		setDate_last_updated(unixTime);
 	}
 
 	public long getTotalElements() {
@@ -44,4 +50,11 @@ public class MetaData {
 		return page.getNumber();
 	}
 
+	public long getDate_last_updated() {
+		return date_last_updated;
+	}
+
+	public void setDate_last_updated(long date_last_updated) {
+		this.date_last_updated = date_last_updated;
+	}
 }
